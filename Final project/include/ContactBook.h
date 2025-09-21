ea5777e18f4a0744a85c9c95e831d188fafcd5a8
Roma4444-44 <marinaeshak9876@gmail.com>
2025-09-21 13:18:25 +0300

    Add files via upload

---
+++ Final project/include/ContactBook.h
@@ -0,0 +1,60 @@
+#ifndef CONTACTBOOK_H
+#define CONTACTBOOK_H
+#include <iostream>
+#include <PhoneNumber.h>
+#include <Contact.h>
+using namespace std;
+class ContactBook
+{
+private:
+    vector<Contact> contacts;
+
+public:
+    void addContact(const Contact &c)
+    {
+        contacts.push_back(c);
+    }
+    void deleteContact(string fname, string lname)
+    {
+        for (auto it = contacts.begin(); it != contacts.end(); ++it)
+        {
+            if (it->getFirstName() == fname && it->getLastName() == lname)
+            {
+                contacts.erase(it);
+                cout << "Contact deleted!\n";
+                return;
+            }
+        }
+        cout << "Contact not found!\n";
+    }
+
+
+    void searchContact(string fname, string lname)
+    {
+        for (const auto &c : contacts)
+        {
+            if (c.getFirstName() == fname && c.getLastName() == lname)
+            {
+                cout << "Contact found:\n";
+                c.display();
+                return;
+            }
+        }
+        cout << "Contact not found!\n";
+    }
+
+    void displayAll() const
+    {
+        if (contacts.empty())
+        {
+            cout << "Contact book is empty.\n";
+            return;
+        }
+        cout << "All Contacts:\n";
+        for (const auto &c : contacts)
+        {
+            c.display();
+        }
+    }
+};
+#endif
